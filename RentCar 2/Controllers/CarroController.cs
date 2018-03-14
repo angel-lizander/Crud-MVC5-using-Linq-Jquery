7e964664a4ac5abf1ab28cd8c5d8c56b25a9c79c
Angel Lizander <31111274+angellizander@users.noreply.github.com>
2018-03-14 01:41:53 -0400

    Primer controlador Agregar autos
    En este primer commit agregamos el crud para agregar autos al rentcar.

---
+++ RentCar 2/Controllers/CarroController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentCar_2.Models;
+
+namespace RentCar_2.Controllers
+{
+    public class CarroController : Controller
+    {
+        RentCarDBDataContext db = new RentCarDBDataContext();
+        // GET: Carro
+        public ActionResult Index()
+        {
+            LoadDropDownsData();
+            var listar = db.Carros.ToList();
+            return View(listar);
+        }
+
+        // GET: Carro/Details/5
+        public ActionResult Details(int id)
+        {
+            var detalles = db.Carros.Single(x => x.IDCarro == id);
+            return View(detalles);
+        }
+
+        // GET: Carro/Create
+        public ActionResult Create()
+        {
+            LoadDropDownsData();
+            return View();
+        }
+
+        void LoadDropDownsData()
+        {
+            var modelo = db.Modelos.ToList().Select(r => new SelectListItem
+            
+            {
+                Value = $"{r.IDModelo}",
+                Text = $"{r.NombreModelo}"
+            });
+
+            ViewBag.model = modelo;
+
+        }
+
+        // POST: Carro/Create
+        [HttpPost]
+        public ActionResult Create(Carro carro)
+        {
+            try
+            {
+                db.Carros.InsertOnSubmit(carro);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Carro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            LoadDropDownsData();
+           var carro = db.Carros.Single(c => c.IDCarro == id);
+            return View(carro);
+        }
+
+        // POST: Carro/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Carro collection)
+        {
+            if(ModelState.IsValid)
+            {
+                // TODO: Add update logic here
+                Carro carro = db.Carros.Single(c => c.IDCarro == id);
+                carro.Matricula = collection.Matricula;
+                carro.ModeloID = collection.ModeloID;
+                carro.Color = collection.Color;
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET: Carro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var ID = db.Carros.Single(x => x.IDCarro == id);
+            return View(ID);
+        }
+
+        // POST: Carro/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Carro collection)
+        {
+            try
+            {
+
+                var eliminar = db.Carros.Single(x => x.IDCarro == id);
+                db.Carros.DeleteOnSubmit(eliminar);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RentCar 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentCar_2.Startup))]
+namespace RentCar_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
